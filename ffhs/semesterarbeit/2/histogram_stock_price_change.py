b0ddab8dcab0def2335cbc6bb45339a5ba4b9a0c
Beat Kappert <beat.kappert@gmail.com>
2019-04-09 08:41:52 +0200

    Semesterarbeit Teil 2

--- ffhs/semesterarbeit/2/histogram_stock_price_change.py
+++ ffhs/semesterarbeit/2/histogram_stock_price_change.py
@@ -41,7 +41,7 @@ bins = [-0.08, -0.07, -0.06, -0.05, -0.04, -0.03, -0.02, -0.01,
 
 plt.rcParams["figure.figsize"] = (10, 4)  # display 10 inches wide, 4 high
 
-plt.title('Histogram of daily price changes')
+plt.title('Histogram of Daily Price Changes')
 plt.hist(daily_price_changes,
          bins=bins,
          rwidth=0.8,
